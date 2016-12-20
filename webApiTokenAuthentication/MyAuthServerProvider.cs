105c1d023d8c3df47db568455be16a804abd8a2f
itsmewaqar <WaqarAh1@LHE-SP-IT77L>
2016-12-20 11:57:44 +0500

    Add project files.

---
+++ webApiTokenAuthentication/MyAuthServerProvider.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace webApiTokenAuthentication
+{
+    public class MyAuthServerProvider : OAuthAuthorizationServerProvider 
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // return base.ValidateClientAuthentication(context);
+
+            //When the client is validated.
+            context.Validated(); // I have validated the client.
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // return base.GrantResourceOwnerCredentials(context);
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Waqar Ahmed"));
+                context.Validated(identity);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Some user"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid grant", "Provided username and pass is incorrect.");
+                return;
+            }
+
+        }
+    }
+}
